1f91a073237be35587c0137af6fe164a733367e2
makasn <sunny.2418.1023@gmail.com>
2018-04-19 23:25:49 +0900

    :+1: add modal to create task

--- resources/coreui/src/views/Tasks.vue
+++ resources/coreui/src/views/Tasks.vue
@@ -1,12 +1,26 @@
 <template>
   <div class="animated fadeIn">
+    <div class="mb-4">
+      <i class="fa fa-pencil-square-o fa-lg fa-2x" @click="largeModal = true"></i>
+    </div>
     <b-row>
       <b-col v-for="task in tasks" :key="task.id" sm="6" md="4">
         <b-card :header="task.name">
           テスト用カード
         </b-card>
       </b-col>
-    </b-row><!--/.row-->
+    </b-row>
+    <b-modal title="Create Task" size="lg" v-model="largeModal" @ok="largeModal = false" hide-footer = true>
+      <b-input-group class="mb-3">
+        <b-input-group-prepend><b-input-group-text><i class="icon-user"></i></b-input-group-text></b-input-group-prepend>
+        <input id="name" type="name" class="form-control" v-model="name" placeholder="name" required autofocus>
+      </b-input-group>
+      <b-row>
+        <b-col cols="6">
+          <b-button @click="create" type="submit" variant="primary" class="px-4">Create</b-button>
+        </b-col>
+      </b-row>
+    </b-modal>
   </div>
 </template>
 
@@ -26,6 +40,7 @@ export default {
       showAlert: false,
       alertMessage: '',
       userState: userStore.state,
+      largeModal: false,
     }
   },
   methods: {
